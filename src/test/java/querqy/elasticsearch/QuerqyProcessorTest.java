cf58420360945a7cdb9af74d4616c627f89876d3
René Kriegler <post@rene-kriegler.com>
2020-08-23 11:13:51 +0200

    Update to querqy-lucene v4.10.lucene810

--- src/test/java/querqy/elasticsearch/QuerqyProcessorTest.java
+++ src/test/java/querqy/elasticsearch/QuerqyProcessorTest.java
@@ -9,6 +9,7 @@ import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.DisjunctionMaxQuery;
+import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,6 +18,7 @@ import querqy.lucene.LuceneQueries;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class QuerqyProcessorTest {
@@ -24,12 +26,11 @@ public class QuerqyProcessorTest {
 
     @Test
     public void testThatAppendFilterQueriesForNullFilterQueries() {
-
         final LuceneQueries queries = new LuceneQueries(
                 new TermQuery(new Term("f1", "a")),
                 null,
-                Collections.singletonList(new TermQuery(new Term("f1", "boost"))), new TermQuery(new Term("f1", "a")),
-                false);
+                Collections.singletonList(new TermQuery(new Term("f1", "boost"))),
+                new TermQuery(new Term("f1", "a")), null, false, false);
 
         final BooleanQuery.Builder builder = new BooleanQuery.Builder();
 
@@ -49,7 +50,7 @@ public class QuerqyProcessorTest {
                 new TermQuery(new Term("f1", "a")),
                 Collections.emptyList(),
                 Collections.singletonList(new TermQuery(new Term("f1", "boost"))), new TermQuery(new Term("f1", "a")),
-                false);
+                null, false, false);
 
         final BooleanQuery.Builder builder = new BooleanQuery.Builder();
 
@@ -74,7 +75,7 @@ public class QuerqyProcessorTest {
                 new TermQuery(new Term("f1", "u")),
                 Collections.singletonList(container.build()),
                 Collections.singletonList(new TermQuery(new Term("f1", "boost"))), new TermQuery(new Term("f1", "u")),
-                false);
+                null, false, false);
 
         final BooleanQuery.Builder builder = new BooleanQuery.Builder();
         builder.add(new TermQuery(new Term("f1", "u")), BooleanClause.Occur.MUST);
