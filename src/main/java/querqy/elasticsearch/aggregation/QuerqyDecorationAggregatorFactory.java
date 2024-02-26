0239fef382c7d58b9cc54bff81253c17ccec8141
paulb <paul@search-solutions.net>
2024-02-26 16:53:02 +0100

    Add test with inner queries

--- src/main/java/querqy/elasticsearch/aggregation/QuerqyDecorationAggregatorFactory.java
+++ src/main/java/querqy/elasticsearch/aggregation/QuerqyDecorationAggregatorFactory.java
@@ -1,5 +1,7 @@
 package querqy.elasticsearch.aggregation;
 
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.ConstantScoreQuery;
 import org.apache.lucene.search.Query;
 import org.elasticsearch.search.aggregations.AggregationExecutionException;
 import org.elasticsearch.search.aggregations.Aggregator;
@@ -10,6 +12,7 @@ import org.elasticsearch.search.aggregations.support.AggregationContext;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -42,10 +45,31 @@ public class QuerqyDecorationAggregatorFactory extends AggregatorFactory {
             throw new AggregationExecutionException("Aggregation [" + name() + "] must have cardinality 1 but was [" + cardinality + "]");
         }
         Query query = context.subSearchContext() == null ? null : context.subSearchContext().query();
-        Set<Object> decorations =
-                (query instanceof DecoratedQuery<?>) ?
-                        ((DecoratedQuery<?>) query).getDecorations() :
-                        Collections.emptySet();
+        Set<DecoratedQuery<?>> decoratedQueries = getDecoratedQueries(query);
+        Set<Object> decorations;
+        if (decoratedQueries.isEmpty()) {
+            decorations = Collections.emptySet();
+        } else {
+            decorations = new HashSet<>();
+            decoratedQueries.forEach(decoratedQuery -> decorations.addAll(decoratedQuery.getDecorations()));
+        }
         return new QuerqyDecorationAggregator(name, context, metadata, decorations);
+    }
+
+    private Set<DecoratedQuery<?>> getDecoratedQueries(Query query) {
+        if (query instanceof DecoratedQuery) {
+            return Collections.singleton((DecoratedQuery<?>) query);
+        }
+        if (query instanceof ConstantScoreQuery) {
+            return getDecoratedQueries(((ConstantScoreQuery) query).getQuery());
+        }
+        if (query instanceof BooleanQuery) {
+            Set<DecoratedQuery<?>> decoratedQueries = new HashSet<>();
+            ((BooleanQuery) query).clauses().forEach(
+                    booleanClause -> decoratedQueries.addAll(getDecoratedQueries(booleanClause.getQuery()))
+            );
+            return decoratedQueries;
+        }
+        return Collections.emptySet();
     }
 }
--- src/test/java/querqy/elasticsearch/aggregation/QuerqyDecorationAggregationIntegrationTest.java
+++ src/test/java/querqy/elasticsearch/aggregation/QuerqyDecorationAggregationIntegrationTest.java
@@ -5,8 +5,13 @@ import org.apache.logging.log4j.Logger;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.WriteRequest;
+import org.elasticsearch.common.bytes.BytesArray;
+import org.elasticsearch.common.xcontent.XContentHelper;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.ConstantScoreQueryBuilder;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.test.ESSingleNodeTestCase;
+import org.elasticsearch.xcontent.XContentType;
 import org.junit.After;
 import querqy.elasticsearch.QuerqyPlugin;
 import querqy.elasticsearch.QuerqyProcessor;
@@ -20,10 +25,15 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 public class QuerqyDecorationAggregationIntegrationTest extends ESSingleNodeTestCase {
 
+    public static String DECORATIONS_TEMPLATE = "{\"decorations\":{\"value\":[%s]}}";
     private final static Logger LOGGER = LogManager.getLogger(QuerqyDecorationAggregationIntegrationTest.class);
 
     private final String INDEX_NAME = "test_index";
@@ -51,43 +61,72 @@ public class QuerqyDecorationAggregationIntegrationTest extends ESSingleNodeTest
         client().execute(PutRewriterAction.INSTANCE, request).get();
 
         QuerqyProcessor querqyProcessor = getInstanceFromNode(QuerqyProcessor.class);
+
+        SearchRequestBuilder searchRequestBuilder = client().prepareSearch(INDEX_NAME);
+
         QuerqyQueryBuilder querqyQuery = new QuerqyQueryBuilder(querqyProcessor);
         querqyQuery.setRewriters(Collections.singletonList(new Rewriter("common_rules")));
-        querqyQuery.setMatchingQuery(new MatchingQuery("a k"));
         querqyQuery.setQueryFieldsAndBoostings(Arrays.asList("field1", "field2"));
         querqyQuery.setMinimumShouldMatch("1");
 
         QuerqyDecorationAggregationBuilder aggregationBuilder = new QuerqyDecorationAggregationBuilder();
+        String[] expectedDecoration = new String[] {"REDIRECT /faq/a","REDIRECT /faq/y"};
+
         // with decorations
-        SearchRequestBuilder searchRequestBuilder = client().prepareSearch(INDEX_NAME);
+        querqyQuery.setMatchingQuery(new MatchingQuery("a k"));
         searchRequestBuilder.setQuery(querqyQuery);
         searchRequestBuilder.addAggregation(aggregationBuilder);
-        SearchResponse response = client().search(searchRequestBuilder.request()).get();
-        assertEquals(2L, response.getHits().getTotalHits().value);
-        assertEquals("{\"decorations\":{\"value\":[\"REDIRECT /faq/a\"]}}", response.getAggregations().getAsMap().get(QuerqyDecorationAggregationBuilder.NAME).toString());
-        LOGGER.info("Response:\n{}", response);
+        testSearchRequest(searchRequestBuilder, 2L, Collections.singleton(expectedDecoration[0]));
 
         // without hits, without decorations
         querqyQuery.setMatchingQuery(new MatchingQuery("x z"));
         searchRequestBuilder.setQuery(querqyQuery);
-        response = client().search(searchRequestBuilder.request()).get();
-        assertEquals(0L, response.getHits().getTotalHits().value);
-        assertEquals("{\"decorations\":{\"value\":[]}}", response.getAggregations().getAsMap().get(QuerqyDecorationAggregationBuilder.NAME).toString());
+        testSearchRequest(searchRequestBuilder, 0L, Collections.emptySet());
 
         // without hits, with decorations
         querqyQuery.setMatchingQuery(new MatchingQuery("x y"));
         searchRequestBuilder.setQuery(querqyQuery);
-        response = client().search(searchRequestBuilder.request()).get();
-        assertEquals(0L, response.getHits().getTotalHits().value);
-        assertEquals("{\"decorations\":{\"value\":[\"REDIRECT /faq/y\"]}}", response.getAggregations().getAsMap().get(QuerqyDecorationAggregationBuilder.NAME).toString());
+        testSearchRequest(searchRequestBuilder, 0L, Collections.singleton(expectedDecoration[1]));
 
         // with hits, without decorations
         querqyQuery.setMatchingQuery(new MatchingQuery("k x"));
         searchRequestBuilder.setQuery(querqyQuery);
-        response = client().search(searchRequestBuilder.request()).get();
-        assertEquals(2L, response.getHits().getTotalHits().value);
-        assertEquals("{\"decorations\":{\"value\":[]}}", response.getAggregations().getAsMap().get(QuerqyDecorationAggregationBuilder.NAME).toString());
+        testSearchRequest(searchRequestBuilder, 2L, Collections.emptySet());
 
+        // inner boolean query
+        querqyQuery.setMatchingQuery(new MatchingQuery("a k"));
+        BoolQueryBuilder boolQueryBuilder = new BoolQueryBuilder();
+        boolQueryBuilder.should(querqyQuery);
+        searchRequestBuilder.setQuery(boolQueryBuilder);
+        testSearchRequest(searchRequestBuilder, 2L, Collections.singleton(expectedDecoration[0]));
+
+        // inner constant score && inner bool query
+        querqyQuery.setMatchingQuery(new MatchingQuery("a k"));
+
+        QuerqyQueryBuilder querqyQuery2 = new QuerqyQueryBuilder(querqyProcessor);
+        querqyQuery2.setRewriters(Collections.singletonList(new Rewriter("common_rules")));
+        querqyQuery2.setQueryFieldsAndBoostings(Arrays.asList("field1", "field2"));
+        querqyQuery2.setMinimumShouldMatch("1");
+        querqyQuery2.setMatchingQuery(new MatchingQuery("x y"));
+
+        ConstantScoreQueryBuilder constantScoreQueryBuilder = new ConstantScoreQueryBuilder(querqyQuery2);
+        boolQueryBuilder = new BoolQueryBuilder();
+        boolQueryBuilder.should(querqyQuery);
+        boolQueryBuilder.should(constantScoreQueryBuilder);
+        searchRequestBuilder.setQuery(boolQueryBuilder);
+        testSearchRequest(searchRequestBuilder, 2L, new HashSet<>(Arrays.asList(expectedDecoration)));
+
+    }
+
+    private void testSearchRequest(SearchRequestBuilder searchRequestBuilder, long expectedHits, Set<Object> expectedDecorations) throws ExecutionException, InterruptedException {
+        SearchResponse response = client().search(searchRequestBuilder.request()).get();
+        LOGGER.info("Response:\n{}", response);
+        assertEquals(expectedHits, response.getHits().getTotalHits().value);
+        InternalDecorationAggregation aggregation = response.getAggregations().get(QuerqyDecorationAggregationBuilder.NAME);
+        assertEquals(
+                expectedDecorations,
+                new HashSet<>((List) aggregation.aggregation())
+        );
     }
 
     @After
